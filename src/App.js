0ab82b79b17029d0a0d66d078e154108c33db3c9
nivaldohydalgo <nivaldohydalgo@gmail.com>
2020-07-12 21:37:20 -0300

    Commit 2020-07-12 21-37

--- src/App.js
+++ src/App.js
@@ -1,22 +1,150 @@
 import React from 'react';
 import './App.css';
 
+import devIcon from "./assets/dev-Icon.png"
+
 function App() {
     return (
         <div className="App">
-            
-            <div className="App-Top">
-                Top
+
+            <div className="App-Left">
+                <div className="App-Icon-Dev">
+                    <img className="Image-Icon-Dev" src={devIcon}></img>
+                </div>
+                <div className="App-Left-Name">
+                    devDicas
+                </div>
+                <div className="App-Left-Icons">
+                    <span class="material-icons Image-Icon-Menu">menu</span>    
+                </div>
             </div>
 
+            <div className="App-Right">
+                <div>Right 001</div>
+                <div>Right 001</div>
+                <div>Right 003</div>
+                <div>Right 004</div>
+                <div>Right 005</div>
+                <div>Right 006</div>
+                <div>Right 007</div>
+                <div>Right 008</div>
+                <div>Right 009</div>
+                <div>Right 010</div>
+                <div>Right 001</div>
+                <div>Right 001</div>
+                <div>Right 003</div>
+                <div>Right 004</div>
+                <div>Right 005</div>
+                <div>Right 006</div>
+                <div>Right 007</div>
+                <div>Right 008</div>
+                <div>Right 009</div>
+                <div>Right 010</div>
+                <div>Right 001</div>
+                <div>Right 001</div>
+                <div>Right 003</div>
+                <div>Right 004</div>
+                <div>Right 005</div>
+                <div>Right 006</div>
+                <div>Right 007</div>
+                <div>Right 008</div>
+                <div>Right 009</div>
+                <div>Right 010</div>
+                <div>Right 001</div>
+                <div>Right 001</div>
+                <div>Right 003</div>
+                <div>Right 004</div>
+                <div>Right 005</div>
+                <div>Right 006</div>
+                <div>Right 007</div>
+                <div>Right 008</div>
+                <div>Right 009</div>
+                <div>Right 010</div>
+            </div>
+{/**
+            <div className="App-Top">
+                <div className="App-Top-Left">
+                    <div className="App-Top-Icon">
+                        <img className="Icon-Image" src={devIcon}></img>
+                            
+                    </div>
+                </div>
+                <div className="App-Top-Right">
+                    <div className="App-Top-Right-Menu">
+                        Top
+                    </div>
+                    <div className="App-Top-Right-Name">
+                        devDicas
+                    </div>
+                </div>
+            </div>
+
+
+        
+
             <div className="App-Middle">
-                Meio
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
+                <div>Conteudo</div>
             </div>
 
             <div className="App-Bottom">
                 Bottom
             </div>
-            
+ */}            
         </div>
     );
 }
